c5f3154b3274cd1711acf4f47970cb72da73871e
Megan Ennis <Megan@Megans-iMac-9.local>
2020-03-20 09:15:02 +0000

    Add algorithm to make player walk in dft and log path til they reach a dead end. Add queue class to be used in bfs when dead end reached

--- adv.py
+++ adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from queue import Queue
 
 import random
 from ast import literal_eval
@@ -44,10 +45,8 @@ for exit in player.current_room.get_exits():
 def get_unexplored_paths(room):
     for exit in room:
         if room[exit] == '?':
-            print(f'exit {exit} is unexplored')    
             return exit
         else:
-            print(f'exit {exit} is explored') 
             return None   
 
 # Step 3: Add helper function that returns whether there are still rooms in the graph that have not been visited.
@@ -58,6 +57,35 @@ def unexplored_paths_exist(graph):
         if '?' in graph[room].values():
             return True
         else: False
+
+reverse_directions = { 'n': 's', 's': 'n', 'e': 'w', 'w': 'e' }
+
+# Step 4: While the graph is unexplored we want to continue traversing it:
+while unexplored_paths_exist(graph):
+    # save current room in variable
+    current_room = player.current_room
+    # if there are unexplored paths in the graph dictionary at the index of the current room..  
+    direction = get_unexplored_paths(graph[current_room.id])
+    if direction is not None:
+        # get the room in that direction by calling the get_room_in_direction() method on room class
+        next_room = current_room.get_room_in_direction(direction)
+        # add the id of the room in that direction to the graph dictionary at the index of the current room
+        graph[current_room.id][direction] = next_room.id
+        # check if next room is already in graph and if not add it..
+        if not next_room.id in graph:
+            graph[next_room.id] = {}
+            # get exits for next room, initialise to '?'
+            for exit in next_room.get_exits():
+                graph[next_room.id][exit] = '?'
+            # add current room id in the reverse direction of where you have to go to get to the next room
+            graph[next_room.id][reverse_directions[direction]] = current_room.id
+        # travel there
+        player.travel(direction)        
+        # and add the direction to the traversal_path
+        traversal_path.append(direction)
+
+
+
 
 
 # TRAVERSAL TEST
