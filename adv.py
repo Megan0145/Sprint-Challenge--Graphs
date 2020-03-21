5810239d6c49d382ddeaa391dc26a211e434586c
Megan Ennis <Megan@Megans-iMac-9.local>
2020-03-21 17:37:56 +0000

    MVP: 1007 moves. Remove redundant helper functions

--- adv.py
+++ adv.py
@@ -12,10 +12,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -43,25 +43,17 @@ for exit in player.current_room.get_exits():
    
 # Step 2: Add function to get unexplored paths from player's current room
 def get_unexplored_paths(room):
+    direction = None
     for exit in room:
         if room[exit] == '?':
-            return exit
-        else:
-            return None   
-
-# Step 3: Add helper function that returns whether there are still rooms in the graph that have not been visited.
-# This can be found by looping over the graph dictionary. For every room in the graph, if the dictionary holding the exits for that room contains a '?', then there are still 
-# paths left unexplored in the graph
-def unexplored_paths_exist(graph):
-    for room in graph:
-        if '?' in graph[room].values():
-            return True
-        else: False
+           direction = exit
+           break
+    return direction       
 
 reverse_directions = { 'n': 's', 's': 'n', 'e': 'w', 'w': 'e' }
 
 # Step 4: While the graph is unexplored we want to continue traversing it:
-while unexplored_paths_exist(graph):
+while len(world.rooms) > len(graph):
     # save current room in variable
     current_room = player.current_room
     # if there are unexplored paths in the graph dictionary at the index of the current room..  
@@ -77,34 +69,91 @@ while unexplored_paths_exist(graph):
             # get exits for next room, initialise to '?'
             for exit in next_room.get_exits():
                 graph[next_room.id][exit] = '?'
-            # add current room id in the reverse direction of where you have to go to get to the next room
-            graph[next_room.id][reverse_directions[direction]] = current_room.id
+        # add current room id in the reverse direction of where you have to go to get to the next room
+        graph[next_room.id][reverse_directions[direction]] = current_room.id
         # travel there
-        player.travel(direction)        
+        player.travel(direction)  
         # and add the direction to the traversal_path
         traversal_path.append(direction)
+    # else we have reached a dead end - we're in a room that has no unexplored paths and need to perform a bfs to find the nearest room with unexplored paths
+    else:   
+        # initialise the queue
+        q = Queue()
+        # enqueue path initially containing the id of the current room to the queue
+        q.enqueue([current_room.id])
+        # initalise visited dictionary to store rooms that have already been visited
+        visited = set()
+        # while queue is not empty
+        while q.size() > 0:
+            # dequeue first path in queue 
+            path = q.dequeue()
+            # get the room id from the last element in the path
+            room_id = path[-1]       
+            # check if there are any unexplored paths to visit in current room
+            direction = get_unexplored_paths(graph[room_id])
+            # if it returns None then we are in a room with no unexplored paths ...
+            if direction is None:
+                # if the room hasn't already been visited, add it to the visited set
+                if room_id not in visited:
+                    # add it to the visited set
+                    visited.add(room_id)
+                    # for every exit in the current room..
+                    for exit in graph[room_id]:
+                        # copy over the current contents of the path
+                        new_path = list(path)
+                        # append the id at that exit to the new path
+                        new_path.append(graph[room_id][exit])
+                        # add the new path to the queue
+                        q.enqueue(new_path)
+            
+            # else there is an unexplored path at the current room        
+            else:
+                # we want to break out of bfs but first will need to convert room ids in path to directions:
+                # declare empty list to hold directions  
+                directions = []       
+                # loop in range 1 to the length of the path 
+                for i in range(0, len(path) - 1):
+                    # if the room in the graph dictionary with the id of the current value of i has a room to the south with id of the room id at i + 1..
+                    if graph[path[i]].get('s') is not None and graph[path[i]].get('s') == path[i + 1]:
+                        # append 's' to directions list
+                        directions.append('s')
+                    # if the room in the graph dictionary with the id of the current value of i has a room to the east with id of the room id at i + 1..    
+                    if graph[path[i]].get('e') is not None and graph[path[i]].get('e') == path[i + 1]:
+                        # append 'e' to directions list
+                        directions.append('e')  
+                    # if the room in the graph dictionary with the id of the current value of i has a room to the west with id of the room id at i + 1..     
+                    if graph[path[i]].get('w') is not None and graph[path[i]].get('w') == path[i + 1]:
+                        # append 'w' to directions list
+                        directions.append('w') 
+                    # if the room in the graph dictionary with the id of the current value of i has a room to the north with id of the room id at i + 1..     
+                    if graph[path[i]].get('n') is not None and graph[path[i]].get('n') == path[i + 1]:
+                        # append 'n' to directions list
+                        directions.append('n')  
+                # for every direction in directions list..
+                for d in directions: 
+                    # travel in that direction
+                    player.travel(d)
 
-
-
-
+                # extend the traversal path list with the direcitons directions list     
+                traversal_path.extend(directions)
+                # break out of bfs 
+                break                     
 
 # TRAVERSAL TEST
-# visited_rooms = set()
-# player.current_room = world.starting_room
-# visited_rooms.add(player.current_room)
+visited_rooms = set()
+player.current_room = world.starting_room
+visited_rooms.add(player.current_room)
 
-# for direct in player.current_room:
-#     print(player.current_room[direct])
+for move in traversal_path:
+    print(move)
+    player.travel(move)
+    visited_rooms.add(player.current_room)
 
-# for move in traversal_path:
-#     player.travel(move)
-#     visited_rooms.add(player.current_room)
-
-# if len(visited_rooms) == len(room_graph):
-#     print(f'TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited')
-# else:
-#     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
-#     print(f'{len(room_graph) - len(visited_rooms)} unvisited rooms')
+if len(visited_rooms) == len(room_graph):
+    print(f'TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited')
+else:
+    print("TESTS FAILED: INCOMPLETE TRAVERSAL")
+    print(f'{len(room_graph) - len(visited_rooms)} unvisited rooms')
 
 
 
